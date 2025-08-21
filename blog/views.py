7919b90eefb60cdef4260f40fc4f66c4f2a13b51
VladimirLevikov <v.v.levikov@gmail.com>
2025-08-21 20:56:12 +0300

    stabilization with comments

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,18 @@ def serialize_post(post):
         'first_tag_title': post.tags.all()[0].title,
     }
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
 
 def serialize_tag(tag):
     return {
@@ -29,16 +41,18 @@ def serialize_tag(tag):
 
 
 def index(request):
-    most_popular_posts = Post.objects.annotate(post_likes=Count('likes')).prefetch_related("author").order_by('-post_likes')[:5]
-    fresh_posts = Post.objects.prefetch_related("author").order_by('published_at')
+    most_popular_posts = Post.objects.annotate(post_likes=Count('likes'),
+                                               post_comments=Count("comments"))\
+                             .prefetch_related("author").order_by('-post_likes')[:5]
+    fresh_posts = Post.objects.annotate(post_comments=Count("comments")).prefetch_related("author").order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
     most_popular_tags = Tag.objects.annotate(posts_tags=Count("posts")).order_by('-posts_tags')[:5]
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
